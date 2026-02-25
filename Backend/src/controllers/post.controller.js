90808ed13d02071174e13222b3ca949703bdda58
pavandantu18 <pavandantu18@gmail.com>
2026-02-24 20:47:41 -0500

    Completed Post Creation feature

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const Imagekit = require("@imagekit/nodejs")
 const {toFile} = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
 const imagekit = new Imagekit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
@@ -9,12 +10,40 @@ const imagekit = new Imagekit({
 async function createPostController(req,res) {
     console.log(req.body,req.file);
 
+    const token = req.cookies.token
+    if(!token) {
+        return res.status(401).json({
+            message: "Token not provided, unauthorized access"
+        })
+    }
+
+    let decoded = null
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded);
+    } catch(e) {
+        return res.status(401).json({
+            message: "User not authorized"
+        })
+    }
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "insta-clone-posts"
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
 }
 
 module.exports = {
